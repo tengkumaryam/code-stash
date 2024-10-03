9617f343a458dfec9ee5fa784402660d5109a238
tengkumaryam <mbintitengku-masrulhisham@boccard.fr>
2024-10-03 15:40:14 +0000

    commented?

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,60 +1,49 @@
 //implémentation Axios
 const axios = require('axios');
 
-// /**
-//  * @return {Promise<void>}
-//  */
-// async function log(message) {
-//     console.log(message);
-//     return "Done !";
-// }
+async function log(message) { // always returns a promise
+    console.log(message);
+    return "Done!";
+}
 
-// // utilisation de API
-// fetch('https://jsonplaceholder.typicode.com/users')
-// .then(response => response.json())
-// .then(async (json) => await log(json))
-// .catch(error => console.log(error));
-
-// fetch('https://jsonplaceholder.typicode.com/users')
-// .then(function(response) {
-//     return response.json()
-// })
-// .then(json => console.log(json))
-// .catch(error => console.log(error));
-
-function timer() {
+async function timer() {
     return new Promise((resolve) => {
-        console.log('waiting for 1s')
+        console.log('Timer activated - waiting for 1s')
         setTimeout(resolve, 1000)
     })
 }
 
-// requête GET
+// utilisation de fetch
+fetch('https://jsonplaceholder.typicode.com/users') // collects data from API, fetch() always returns a promise, so program goes on w/o waiting for result
+    .then(response => response.json())
+    .then((json) => console.log(json))
+
+// utilisation de axios (old ver of fetch)
 async function main() {
-    let withErrors = false;
+    // let withErrors = false;
 
     try {
-        await timer()
+        await timer() // await can only be used in async func, pauses execution of func until the promise is resolved or rejected
             .then(() => axios.get('https://jsonplaceholder.typicode.com/posts?id=3'))
             .then(response => response.data)
-            .then(async function (data) {
-                console.log('reponse received')
+            .then(async function (data) { //anonymous func, for one-off usage
+                console.log('Response with async');
 
                 await timer();
-                console.log('response with await', data);
+                console.log('Response with await', data);
 
-                return timer().then(() => console.log('response with then', data));
+                // return timer().then(() => console.log('response with then', data));
             })
             .then(() => new Promise((_, reject) => reject(new Error('Boom !'))))
             .catch(function (error) {
-                withErrors = true;
+                // withErrors = true;
                 console.error('An error occured in the then/catch !', error);
-                throw error;
+                throw error; // bring error up alng the chain, thus catch() displays error too
             });
-    } catch (error2) {
+    }
+    catch (error2) {
         console.error('An error occured in the try/catch !', error2);
     }
 }
-
 
 main();
