8d6098d64d949405441c40f45628cd897bf06119
tengkumaryam <mbintitengku-masrulhisham@boccard.fr>
2024-10-03 13:59:20 +0000

    exercice2 original

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -29,72 +29,31 @@ function timer() {
     })
 }
 
-async function fetchData(url) {
-    console.log('a')
-    try {
-        console.log('b')
-        const response = await axios.get(url, {timeout: 5000});
-        console.log('c')
-        return response.data;
-    } catch(e) {
-        console.log('d')
-        return 'error';
-    }
-}
-
 // requête GET
 async function main() {
     let withErrors = false;
 
-    // try {
-    //     await timer()
-    //         .then(() => axios.get('https://jsonplaceholder.typicode.com/posts?id=3'))
-    //         .then(response => response.data)
-    //         .then(async function (data) {
-    //             console.log('reponse received')
+    try {
+        await timer()
+            .then(() => axios.get('https://jsonplaceholder.typicode.com/posts?id=3'))
+            .then(response => response.data)
+            .then(async function (data) {
+                console.log('reponse received')
 
-    //             await timer();
-    //             console.log('response with await', data);
+                await timer();
+                console.log('response with await', data);
 
-    //             return timer().then(() => console.log('response with then', data));
-    //         })
-    //         .then(() => new Promise((_, reject) => reject(new Error('Boom !'))))
-    //         .catch(function (error) {
-    //             withErrors = true;
-    //             console.error('An error occured in the then/catch !', error);
-    //             throw error;
-    //         });
-    // } catch (error2) {
-    //     console.error('An error occured in the try/catch !', error2);
-    // }
-    const startAt = Date.now();
-
-
-    const urls = Array.from({ length: 100 }).map((val, index) => {
-        return 'https://jsonplaceholder.typicode.com/posts?id=' + index
-    })
-
-    const responses = [];
-    for(const url of urls) {
-        console.log('a')
-        try {
-            console.log('b')
-            const response = await axios.get(url, {timeout: 5000});
-            console.log('c')
-            responses.push(response.data);
-        } catch(e) {
-            console.log('d')
-            responses.push('error');
-        }
+                return timer().then(() => console.log('response with then', data));
+            })
+            .then(() => new Promise((_, reject) => reject(new Error('Boom !'))))
+            .catch(function (error) {
+                withErrors = true;
+                console.error('An error occured in the then/catch !', error);
+                throw error;
+            });
+    } catch (error2) {
+        console.error('An error occured in the try/catch !', error2);
     }
-    console.log('waiting for promises')
-    
-
-    const [avatar, infos, pub] = responses;
-
-    const endAt = Date.now();
-
-    console.log(`done in ${endAt - startAt}ms !`, {avatar, infos, publicite: pub})
 }
 
 
