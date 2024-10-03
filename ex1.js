1751b14dbbeedd821c03a6e388fb263aa3cc7714
tengkumaryam <mbintitengku-masrulhisham@boccard.fr>
2024-10-03 08:30:39 +0000

    finished ver

--- ex1.js
+++ ex1.js
@@ -1,34 +1,35 @@
 const { faker } = require('@faker-js/faker');
 
+// génération des données
 
-// Génération des données
+function donnees(j) {
+    let users = [];
+    for (let i=0; i<j; i++) {
+        console.log('User number', i);
+        users.push({
+            id: faker.number.int({ min: 100, max: 999 }),
+            name: faker.person.lastName(),
+            age: faker.number.int({min:18, max:60}),
+            email: faker.internet.email()
+        });
+    };
+    return users;
+}
 
-const users = [];
-
-for (i=0; i<10; i++) {
-    users.push({
-        id: faker.number.int({ min: 1000, max: 9999 }),
-        name: faker.person.lastName(),
-        age: faker.number.int({min:18, max:60}),
-        email: faker.internet.email()
-    });
-};
-
+var nb = 5;
+const users = donnees(nb);
 console.log("Tableau des utilisateurs :", users);
 
 // map ()
+const nomsMaj = users.map(user => user.name.toUpperCase());
 
 // filter ()
-
 const oldUsers = users.filter((user) => user.age > 30);
-console.log("Les utilisateurs qui ont plus de 30 ans :", oldUsers);
 
 // reduce ()
+const ageTotal = oldUsers.reduce((sum, user) => sum + user.age, 0);
 
-const ageTotal = oldUsers.reduce(
-    
-);
+// affichage
+console.log("Les noms en majuscule :", nomsMaj);
+console.log("Les utilisateurs qui ont plus de 30 ans :", oldUsers);
 console.log("Le somme des âges :", ageTotal);
-
-
-
