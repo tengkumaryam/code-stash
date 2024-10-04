96ac3878d22af264f201a259b5805291a5b1e7b1
tengkumaryam <mbintitengku-masrulhisham@boccard.fr>
2024-10-04 15:11:32 +0000

    done

--- ex4/vue-project/src/components/LesComments.vue
+++ ex4/vue-project/src/components/LesComments.vue
@@ -1,11 +1,32 @@
 <template>
-    <div class="comments">
+    <!-- <div class="comments">
         <h2>Les commentaires</h2>
-        <p>{{comments}}</p>
-
-
+        <p>{{ comments }}</p>
+        <h1>names</h1>
+        <p>{{ names }}</p>
+        <h1>emails</h1>
+        <p>{{ emails }}</p>
+        <h1>bodies</h1>
+        <p>{{ bodies }}</p>
+    </div> -->
+    <div class="container">
+        <div class="row">
+            <div v-for="comment in comments" :key="comment.id" class="col-md-3">
+                <b-card :title="comment.name">
+                    <b-card-text>
+                        <strong>Email:</strong>
+                        <br />
+                        {{ comment.email }}
+                        <br />
+                        <strong>Comment:
+                        </strong>
+                        <br />
+                        {{ comment.body }}
+                    </b-card-text>
+                </b-card>
+            </div>
+        </div>
     </div>
-
 </template>
 
 <script>
@@ -16,7 +37,9 @@ export default {
     data() {
         return {
             comments: [],
-            //error: null,
+            // names: [],
+            // emails: [],
+            // bodies: [],
         };
     },
     created() {
@@ -25,11 +48,19 @@ export default {
     methods: {
         async fetchComments() {
             try {
-                const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
-                this.comments = response.data;
+                // const responses = await axios.get('https://jsonplaceholder.typicode.com/comments');
+                const responses = await axios.get('http://192.168.107.121:3000/comments');
+                this.comments = responses.data;
+                // this.names = this.comments.map(comment => comment.name)
+                // this.emails = this.comments.map(comment => comment.email)
+                // this.bodies = this.comments.map(comment => comment.body)
             } catch (err) {
                 console.error('Error!', err);
             }
+        },
+
+        loadComments() {
+            this.fetchComments();
         },
     },
 };
