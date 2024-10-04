355e7b433184edf204a7bae371e55196bb784fd8
tengkumaryam <mbintitengku-masrulhisham@boccard.fr>
2024-10-04 13:11:57 +0000

    display comments directly

--- ex4/vue-project/src/App.vue
+++ ex4/vue-project/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Maryam's Vue.js App"/>
+    <LesComments/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import LesComments from './components/LesComments.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    LesComments
+  },
+};
 </script>
 
 <style>
---
+++ ex4/vue-project/src/components/LesComments.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="comments">
+        <h2>Les commentaires</h2>
+        <p>{{comments}}</p>
+
+
+    </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'LesComments',
+    data() {
+        return {
+            comments: [],
+            //error: null,
+        };
+    },
+    created() {
+        this.fetchComments();
+    },
+    methods: {
+        async fetchComments() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
+                this.comments = response.data;
+            } catch (err) {
+                console.error('Error!', err);
+            }
+        },
+    },
+};
+</script>
+
+<style>
+.comments {
+    margin: 20px 0;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+</style>
